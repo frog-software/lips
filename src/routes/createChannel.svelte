4b125fae0c91b00edc7a7e6e919764e86de94944
0214 <3614157673@qq.com>
2023-11-26 10:45:12 +0800

    频道的创建

--- src/routes/createChannel.svelte
+++ src/routes/createChannel.svelte
@@ -1,3 +1,30 @@
-<h1>
-    Create your channel
-</h1>
+<script>
+  import { push } from "svelte-spa-router";
+  import PocketBase from "pocketbase";
+
+  const pb = new PocketBase("http://127.0.0.1:8090");
+  let channelName = "";
+
+  async function handleCreate() {
+    const data = {
+            channelName: channelName,
+        };
+    try {
+      await pb.collection("channels").create(data);
+      push("/yourChannel");
+    } catch (error) {
+      alert("ERROR: " + error.message);
+    }
+  }
+</script>
+
+<h1>Create your channel</h1>
+
+<form on:submit|preventDefault={handleCreate}>
+<div class="content-box">
+  <div class="item-box">
+    <input type="text" bind:value={channelName}   placeholder="Channel Name" />
+  </div>
+</div>
+<button type="submit">Create Channel</button>
+</form>
