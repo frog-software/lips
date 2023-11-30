f719d6716e528853387f3c240d64fa0d40b77f0c
cl x <572457468@qq.com>
2023-11-30 15:04:59 +0800

    build: update the CI

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,2 +1,2 @@
- export const PocketBase_URL='http://www.open1ips.cn:8090';
-// export const PocketBase_URL='http://127.0.0.1:8090'; test用
+export const PocketBase_URL='http://10.112.209.162:8090/_/';
+//export const PocketBase_URL='http://127.0.0.1:8090'; //test用
