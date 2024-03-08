2478cdfb1e0a29b1d20aed49d42b65c94d9bc735
独 后藤 <1823773980@qq.com>
2024-03-08 23:25:37 +0800

    feat: click and check all the channels

---
+++ src/routes/Modal.svelte
@@ -0,0 +1,46 @@
+<!-- Modal.svelte -->
+<script>
+  export let isOpen = false; // 控制模态对话框是否显示的变量
+  export let close; // 关闭模态对话框的函数
+</script>
+
+{#if isOpen}
+  <div class="modal-overlay" on:click={close}>
+    <div class="modal-content" on:click|stopPropagation>
+      <button class="close-button" on:click={close}>×</button>
+      <slot />
+      <!-- 用于插入模态对话框的内容 -->
+    </div>
+  </div>
+{/if}
+
+<style>
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal-content {
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    position: relative;
+  }
+
+  .close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-size: 20px;
+  }
+</style>
--- src/routes/checkInformation.svelte
+++ src/routes/checkInformation.svelte
@@ -1,1 +1,61 @@
-<h1>check your information</h1>
+<!-- 查看频道 -->
+<script>
+  import PocketBase from "pocketbase";
+  import { PocketBase_URL } from "../utils/api/index";
+  import Modal from "./Modal.svelte";
+  import { onMount } from "svelte";
+  const pb = new PocketBase(PocketBase_URL);
+  let records = [];
+  let showModal = false;
+  async function checkchan() {
+    try {
+      const response = await pb.collection("channels").getFullList({
+        sort: "-created",
+      });
+      records = response;
+    } catch (error) {
+      alert("fail to find");
+    }
+  }
+  function toggleModal() {
+    showModal = !showModal;
+  }
+  onMount(() => {
+    checkchan();
+  });
+</script>
+
+<h1>check your channels</h1>
+
+<button on:click={toggleModal}>查看频道</button>
+
+<!-- <Modal show={showModal} close={() => showModal = false}>
+    
+</Modal> -->
+<Modal isOpen={showModal} close={toggleModal}>
+  <h2>频道</h2>
+  <div class="container">
+    {#each records as record}
+      <button class="button">#{record.channelName}</button>
+      <!-- <p>#{record.channelName}</p> -->
+    {/each}
+  </div>
+</Modal>
+
+<style>
+  .container {
+    max-width: 300px;
+    max-height: 200px; /* 设置列表的最大高度 */
+    overflow-y: auto; /* 超出部分显示滚动条 */
+    background: #f9f9f9; /* 背景色，可根据需要调整 */
+    border-radius: 5px; /* 边框圆角 */
+    padding: 10px; /* 内边距 */
+  }
+  .button {
+    width: 60%;
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #f8f9fa;
+    border-radius: 4px;
+  }
+</style>
