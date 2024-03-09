b65e397c7e50181df1439d417bc7f3a2a5b6b058
後藤ひとり <118675237+babuzeer@users.noreply.github.com>
2024-03-10 01:13:04 +0800

    eliminate warings (#49)
    * feat: add notices to a new collection
    
    the new collection named todolist, and there are small changes about
    how the pages look like.
    
    * feat: add notices to a new collection
    
    * style: update npm and run format
    
    * style: run npm format again
    
    * feat: link notices, todolists and channels
    
    * feat: update todolist-link
    
    * feat: update
    
    * style: change a little style
    
    * feat: eliminate warnings
    
    ---------
    
    Co-authored-by: sheeplin <1270610465@qq.com>
    Co-authored-by: My Go！ <118411001+Clear1oveE@users.noreply.github.com>

---
+++ src/routes/checknotice.svelte
@@ -0,0 +1,58 @@
+<!-- 显示通知具体内容 -->
+<script>
+  import { currentnoticeid } from "../store.js";
+  import PocketBase from "pocketbase";
+  import { PocketBase_URL } from "../utils/api/index";
+  import { onMount } from "svelte";
+
+  const pb = new PocketBase(PocketBase_URL);
+  let records = [];
+  async function noticedisplay() {
+    try {
+      const createid = $currentnoticeid;
+      const response = await pb.collection("notices").getFullList({
+        sort: "-created",
+        filter: `id="${createid}"`,
+      });
+      records = response;
+    } catch (error) {
+      alert("fail to find");
+    }
+  }
+
+  onMount(() => {
+    noticedisplay();
+  });
+</script>
+
+{#each records as record}
+  <div class="record">
+    <div class="tittle">{record.tittle}</div>
+    <div class="meta">
+      {record.year}.{record.month}.{record.day}/#{record.tag}/from:{record.useremail}
+    </div>
+    <div>{record.body}</div>
+  </div>
+{/each}
+
+<style>
+  .record {
+    border: 1px solid #ccc;
+    padding: 15px;
+    margin: 10px 0;
+    background-color: #f9f9f9;
+  }
+
+  .tittle {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 8px;
+    color: #333;
+  }
+
+  .meta {
+    font-size: 12px;
+    color: #666;
+    margin-bottom: 12px;
+  }
+</style>
