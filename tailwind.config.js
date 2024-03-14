00af94de53f904fbdf3f9159e8dce45ea51268bd
lch6331 <147340789+lch6331@users.noreply.github.com>
2024-03-14 20:43:59 +0800

    feat:add daisyui lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{svelte,js,ts}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [ 'light' ]
+  }
 }
 
