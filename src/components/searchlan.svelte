0bcc05e163c2cae90a2a78bf865834bde064cd69
独 后藤 <1823773980@qq.com>
2024-03-18 16:57:17 +0800

    style: devide the searching part

--- src/components/searchlan.svelte
+++ src/components/searchlan.svelte
@@ -1,44 +1,44 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
-    import { originChannelID, currentnoticeid } from "../store.js";
-    import PocketBase from "pocketbase";
-    import { push } from "svelte-spa-router";
-    import { PocketBase_URL } from "../utils/api/index";
-  
-    const pb = new PocketBase(PocketBase_URL);
-    let searchQuery = "";
-    // let isMainPage = true;
-    let errorMessage = "";
-    let searchchannels = [];
-    let searchnotices = [];
-    let searchtags = [];
-    let isFocused = false;
-  
-    function jumpnew(id) {
-      originChannelID.set(id);
-      push("/chantemplate");
-    }
-    function jumpnewnotice(id) {
-      currentnoticeid.set(id);
-      push("/checknotice");
-    }
-    function debounce(func, wait) {
-      let timeout;
-      return function executedFunction(...args) {
-        const later = () => {
-          clearTimeout(timeout);
-          func(...args);
-        };
+  import { onMount, onDestroy } from "svelte";
+  import { originChannelID, currentnoticeid } from "../store.js";
+  import PocketBase from "pocketbase";
+  import { push } from "svelte-spa-router";
+  import { PocketBase_URL } from "../utils/api/index";
+
+  const pb = new PocketBase(PocketBase_URL);
+  let searchQuery = "";
+  // let isMainPage = true;
+  let errorMessage = "";
+  let searchchannels = [];
+  let searchnotices = [];
+  let searchtags = [];
+  let isFocused = false;
+
+  function jumpnew(id) {
+    originChannelID.set(id);
+    push("/chantemplate");
+  }
+  function jumpnewnotice(id) {
+    currentnoticeid.set(id);
+    push("/checknotice");
+  }
+  function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+      const later = () => {
         clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+        func(...args);
       };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+  function handleClickOutside(event) {
+    if (!event.target.closest(".search")) {
+      isFocused = false;
     }
-    function handleClickOutside(event) {
-      if (!event.target.closest(".search")) {
-        isFocused = false;
-      }
-    }
-      // function isid() {
+  }
+  // function isid() {
   //   if ($currentchannelid != null) isMainPage = false;
   // }
   // function search(){
@@ -97,155 +97,155 @@
 </script>
 
 <div class="search">
-    <input
-      type="text"
-      bind:value={searchQuery}
-      on:input={debouncedSearchChannel}
-      placeholder="search everything..."
-      on:focus={() => (isFocused = true)}
-    />
-    <button on:click={() => search()}>search</button>
-    {#if errorMessage}
-      <div class="searchdrop">
+  <input
+    type="text"
+    bind:value={searchQuery}
+    on:input={debouncedSearchChannel}
+    placeholder="search everything..."
+    on:focus={() => (isFocused = true)}
+  />
+  <button on:click={() => search()}>search</button>
+  {#if errorMessage}
+    <div class="searchdrop">
+      <div
+        class="searchdrop-item"
+        role="button"
+        tabindex="0"
+        on:keypress
+        on:click={() => {
+          isFocused = false;
+        }}
+      >
+        <p>{errorMessage}</p>
+      </div>
+    </div>
+  {:else if isFocused && (searchchannels.length > 0 || searchnotices.length > 0 || searchtags.length > 0) && searchQuery !== ""}
+    <div class="searchdrop">
+      {#each searchchannels as channel}
         <div
           class="searchdrop-item"
           role="button"
           tabindex="0"
           on:keypress
-          on:click={() => {
-            isFocused = false;
-          }}
+          on:click={() => jumpnew(channel.id)}
         >
-          <p>{errorMessage}</p>
+          <div class="channame">
+            <strong>{channel.channelName}</strong>
+            <p class="droptag">频道</p>
+          </div>
+          <p>{channel.channelDescription}</p>
         </div>
-      </div>
-    {:else if isFocused && (searchchannels.length > 0 || searchnotices.length > 0 || searchtags.length > 0) && searchQuery !== ""}
-      <div class="searchdrop">
-        {#each searchchannels as channel}
-          <div
-            class="searchdrop-item"
-            role="button"
-            tabindex="0"
-            on:keypress
-            on:click={() => jumpnew(channel.id)}
-          >
-            <div class="channame">
-              <strong>{channel.channelName}</strong>
-              <p class="droptag">频道</p>
-            </div>
-            <p>{channel.channelDescription}</p>
-          </div>
-        {/each}
-        {#each searchnotices as notice}
-          <div
-            class="searchdrop-item"
-            role="button"
-            tabindex="0"
-            on:keypress
-            on:click={() => jumpnewnotice(notice.id)}
-          >
-            <div class="channame">
-              <strong>{notice.tittle}</strong>
-              <p class="droptag">通知</p>
-            </div>
-            <p>#{notice.tag} 作者:{notice.username}</p>
-          </div>
-        {/each}
-        {#each searchtags as notice}
-          <div
-            class="searchdrop-item"
-            role="button"
-            tabindex="0"
-            on:keypress
-            on:click={() => jumpnewnotice(notice.id)}
-          >
-            <div class="channame">
-              <strong>{notice.tittle}</strong>
-              <p class="droptag">通知</p>
-            </div>
-            <p>#{notice.tag} 作者:{notice.username}</p>
-          </div>
-        {/each}
-      </div>
-    {:else if isFocused && searchQuery !== ""}
-      <div class="searchdrop">
+      {/each}
+      {#each searchnotices as notice}
         <div
           class="searchdrop-item"
           role="button"
           tabindex="0"
           on:keypress
-          on:click={() => {
-            isFocused = false;
-          }}
+          on:click={() => jumpnewnotice(notice.id)}
         >
-          <p>无法显示相关频道或通知。</p>
+          <div class="channame">
+            <strong>{notice.tittle}</strong>
+            <p class="droptag">通知</p>
+          </div>
+          <p>#{notice.tag} 作者:{notice.username}</p>
         </div>
+      {/each}
+      {#each searchtags as notice}
+        <div
+          class="searchdrop-item"
+          role="button"
+          tabindex="0"
+          on:keypress
+          on:click={() => jumpnewnotice(notice.id)}
+        >
+          <div class="channame">
+            <strong>{notice.tittle}</strong>
+            <p class="droptag">通知</p>
+          </div>
+          <p>#{notice.tag} 作者:{notice.username}</p>
+        </div>
+      {/each}
+    </div>
+  {:else if isFocused && searchQuery !== ""}
+    <div class="searchdrop">
+      <div
+        class="searchdrop-item"
+        role="button"
+        tabindex="0"
+        on:keypress
+        on:click={() => {
+          isFocused = false;
+        }}
+      >
+        <p>无法显示相关频道或通知。</p>
       </div>
-    {/if}
-  </div>
+    </div>
+  {/if}
+</div>
 
-  <style>
-    .search {
-      margin-right: 20%;
-      position: relative;
-    }
-    input[type="text"] {
-      width: 400px;
-      padding: 8px;
-      font-size: 16px;
-      border: 2px solid #ddd;
-      border-radius: 16px;
-      outline: none;
-      transition: border-color 0.3s;
-    }
-    input[type="text"]:focus {
-      width: 400px;
-      border-color: #007bff;
-    }
-    button {
-      padding: 8px 15px;
-      font-size: 16px;
-      background-color: #c8c8c8;
-      color: white;
-      border: none;
-      border-radius: 16px;
-      cursor: pointer;
-      margin-left: 10px;
-      outline: none;
-    }
-    button:hover {
-      background-color: #0056b3;
-    }
-    .channame {
-      display: flex; /* 启用Flexbox布局 */
-      align-items: center; /* 垂直居中 */
-      gap: 10px; /* 在项目之间添加一些间隔 */
-    }
-    .droptag {
-      color: #fff;
-      background-color: #7d7d7d;
-      width: 10%;
-      text-align: center;
-      font-size: small;
-    }
-    .searchdrop {
-      position: absolute;
-      top: 100%;
-      background-color: #fff;
-      border: 1px solid #ddd;
-      border-radius: 4px;
-      width: 400px; /* 根据输入框宽度调整 */
-      max-height: 200px;
-      overflow-y: auto;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-      margin-top: 2px;
-    }
-    .searchdrop-item {
-      padding: 10px;
-      cursor: pointer;
-      text-align: left;
-    }
-    .searchdrop-item:hover {
-      background-color: #f5f5f5;
-    }
-  </style>
+<style>
+  .search {
+    margin-right: 20%;
+    position: relative;
+  }
+  input[type="text"] {
+    width: 400px;
+    padding: 8px;
+    font-size: 16px;
+    border: 2px solid #ddd;
+    border-radius: 16px;
+    outline: none;
+    transition: border-color 0.3s;
+  }
+  input[type="text"]:focus {
+    width: 400px;
+    border-color: #007bff;
+  }
+  button {
+    padding: 8px 15px;
+    font-size: 16px;
+    background-color: #c8c8c8;
+    color: white;
+    border: none;
+    border-radius: 16px;
+    cursor: pointer;
+    margin-left: 10px;
+    outline: none;
+  }
+  button:hover {
+    background-color: #0056b3;
+  }
+  .channame {
+    display: flex; /* 启用Flexbox布局 */
+    align-items: center; /* 垂直居中 */
+    gap: 10px; /* 在项目之间添加一些间隔 */
+  }
+  .droptag {
+    color: #fff;
+    background-color: #7d7d7d;
+    width: 10%;
+    text-align: center;
+    font-size: small;
+  }
+  .searchdrop {
+    position: absolute;
+    top: 100%;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    width: 400px; /* 根据输入框宽度调整 */
+    max-height: 200px;
+    overflow-y: auto;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    margin-top: 2px;
+  }
+  .searchdrop-item {
+    padding: 10px;
+    cursor: pointer;
+    text-align: left;
+  }
+  .searchdrop-item:hover {
+    background-color: #f5f5f5;
+  }
+</style>
